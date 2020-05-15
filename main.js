1884becc186c17075224619d61ac424a2e1c3809
fteve96 <ftevenden1@gmail.com>
2020-05-15 01:10:48 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const array = require('./myArray')
+
+function main(){
+  array.SIZE_RATIO + 3
+
+  let arr = new array()
+
+  arr.push(3)
+
+  console.log(arr)
+}
+
+main()
---
+++ myArray.js
@@ -0,0 +1,69 @@
+const memory = require('./memory')
+
+class MyArray {
+  constructor() {
+    this.length = 0
+    this._capacity = 0
+    this.ptr = memory.allocate(this.length)
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * MyArray.SIZE_RATIO)
+    }
+    memory.set(this.ptr + this.length, value)
+    this.length++
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error(`Index error`)
+    }
+    return memory.get(this.ptr + index)
+  }
+
+  pop() {
+    if (this.length == 0){
+      throw new Error(`Index error`)
+    }
+    const value = memory.get(this.ptr + this.length - 1)
+    this.length--
+    return value
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error(`index error`)
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * MyArray.SIZE_RATIO)
+    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index)
+    memory.set(this.ptr + index, value)
+    this.length++
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error(`Index error`)
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index)
+    this.length--
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr
+    this.ptr = allocate(size)
+    if(this.ptr === null) {
+      throw new Error(`Out of memory`)
+    }
+    memory.copy(this.ptr, oldPtr, this.length)
+    memory.free(oldPtr)
+    this._capacity = size
+  }
+}
+
+MyArray.SIZE_RATIO = 3
+
+module.exports = MyArray
